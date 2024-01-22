dd88d17497a4d77b07ec57a217acba5e3b9d0033
Anukul20 <112794506+AnukulNandi@users.noreply.github.com>
2024-01-22 10:59:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+//condition method(2,3):
+function nextPage2()
+{
+if(document.getElementById("pass").type=="password")
+{document.getElementById("pass").type="text";}
+else
+{document.getElementById("pass").type="password";;}
+}
+
+
+
